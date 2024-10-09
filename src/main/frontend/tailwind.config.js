545762af109086800ea5ea2e66d6f873f11e6a3a
Francislainy <francislainy.campos@gmail.com>
2024-10-09 15:20:02 +0100

    Apply tailwind style

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/main/jte/**/*.jte', // Ensure Tailwind scans your JTE files
+    '../jte/**/*.jte', // Ensure Tailwind scans your JTE files
     './src/main/resources/static/**/*.css', // Optionally include other static resources if needed
   ],
   theme: {
